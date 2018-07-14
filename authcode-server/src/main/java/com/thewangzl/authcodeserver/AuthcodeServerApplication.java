be968973a92023af4a6e423fa6f837272690c0e7
thewangzl <thewangzl@gmail.com>
2018-07-15 07:35:19 +0800

    4种授权模式

---
+++ authcode-server/src/main/java/com/thewangzl/authcodeserver/AuthcodeServerApplication.java
@@ -0,0 +1,12 @@
+package com.thewangzl.authcodeserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthcodeServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthcodeServerApplication.class, args);
+	}
+}
